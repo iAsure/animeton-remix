4afb516fb5a6d5a40dfbe5511b6ad0e283698899
Rafael Ayala <asuregames01@gmail.com>
2024-12-18 15:31:42 -0600

    fixed latest episodes styles and animations

--- app/routes/latest-episodes/route.tsx
+++ app/routes/latest-episodes/route.tsx
@@ -1,7 +1,7 @@
 import LatestEpisodes from '@/shared/components/episode/LatestEpisode';
 
 const LatestEpisodesPage = () => (
-  <div className="py-6">
+  <div className="py-6 mx-auto w-full max-w-[90%]">
     <LatestEpisodes perPage={32} showViewMore={false} cardAnimation={true} />
   </div>
 );
